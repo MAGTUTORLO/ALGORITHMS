748342b198facd427f94373b7fa95a7201eb8e57
jamesjumawan <48267843+jamesjumawan@users.noreply.github.com>
2019-11-29 19:49:00 +0800

    Update DFS.cpp

--- DFS.cpp
+++ DFS.cpp
@@ -1,87 +1,66 @@
-// C++ program to print DFS traversal from
-// a given vertex in a given graph
-#include<iostream>
-#include<list>
+#include <bits/stdc++.h>
 using namespace std;
 
-// Graph class represents a directed graph
-// using adjacency list representation
-class Graph
+class Node
 {
-	int V; // No. of vertices
-
-	// Pointer to an array containing
-	// adjacency lists
-	list<int> *adj;
-
-	// A recursive function used by DFS
-	void DFSUtil(int v, bool visited[]);
-public:
-	Graph(int V); // Constructor
-
-	// function to add an edge to graph
-	void addEdge(int v, int w);
-
-	// DFS traversal of the vertices
-	// reachable from v
-	void DFS(int v);
+    public:
+    
+    int index;
+    string name;
+    vector<int> neighbors;
+    bool visited;
+    
+    Node( int i, string n )
+    {
+        index = i;
+        name = n;
+        neighbors.clear();
+        visited = false;
+    }
 };
 
-Graph::Graph(int V)
-{
-	this->V = V;
-	adj = new list<int>[V];
-}
+void DFS(Node[],int);
 
-void Graph::addEdge(int v, int w)
-{
-	adj[v].push_back(w); // Add w to v’s list.
-}
-
-void Graph::DFSUtil(int v, bool visited[])
-{
-	// Mark the current node as visited and
-	// print it
-	visited[v] = true;
-	cout << v << " ";
-
-	// Recur for all the vertices adjacent
-	// to this vertex
-	list<int>::iterator i;
-	for (i = adj[v].begin(); i != adj[v].end(); ++i)
-		if (!visited[*i])
-			DFSUtil(*i, visited);
-}
-
-// DFS traversal of the vertices reachable from v.
-// It uses recursive DFSUtil()
-void Graph::DFS(int v)
-{
-	// Mark all the vertices as not visited
-	bool *visited = new bool[V];
-	for (int i = 0; i < V; i++)
-		visited[i] = false;
-
-	// Call the recursive helper function
-	// to print DFS traversal
-	DFSUtil(v, visited);
-}
-
-// Driver code
 int main()
 {
-	// Create a graph given in the above diagram
-	Graph g(4);
-	g.addEdge(0, 1);
-	g.addEdge(0, 2);
-	g.addEdge(1, 2);
-	g.addEdge(2, 0);
-	g.addEdge(2, 3);
-	g.addEdge(3, 3);
+    Node nodes[] = {Node(0,"A"), Node(1,"B"), Node(2,"C"), Node(3,"D")};
+    
+    nodes[0].neighbors = {1,2,3};
+    nodes[1].neighbors = {0};
+    nodes[2].neighbors = {0,3};
+    nodes[3].neighbors = {0,2};
+    
+    DFS(nodes,2);
+    
+    return 0;
+}
 
-	cout << "Following is Depth First Traversal"
-			" (starting from vertex 2) \n";
-	g.DFS(2);
-
-	return 0;
+void DFS(Node nodes[], int n)
+{
+    bool willPop;
+    stack<Node> s;
+    s.push(nodes[n]);
+    nodes[n].visited = true;
+    cout<<s.top().name<<" ";
+    
+    while(s.size()!=0)
+    {
+        willPop = true;
+        for(long unsigned int i = 0; i < s.top().neighbors.size(); i++)
+        {
+            int current = s.top().neighbors[i];
+            if(nodes[current].visited == false)
+            {
+                nodes[current].visited = true;
+                willPop = false;
+                s.push(nodes[current]);
+                cout<<s.top().name<<" ";
+                break;
+            }
+        }
+        if(willPop == true)
+        {
+            s.pop();
+        }
+    }
 }
