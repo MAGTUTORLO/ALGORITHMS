cd9c2be58ac1a190c05f2de55262c37d55168acb
jamesjumawan <48267843+jamesjumawan@users.noreply.github.com>
2019-11-29 21:14:00 +0800

    Update DFS.cpp

--- DFS.cpp
+++ DFS.cpp
@@ -23,14 +23,19 @@ void DFS(Node[],int);
 
 int main()
 {
-    Node nodes[] = {Node(0,"A"), Node(1,"B"), Node(2,"C"), Node(3,"D")};
+    Node nodes[] = {Node(0,"A"), Node(1,"B"), Node(2,"C"), Node(3,"D"), Node(4,"E"), Node(5,"F"), Node(6,"G"), Node(7,"H"), Node(8,"I")};
     
     nodes[0].neighbors = {1,2,3};
-    nodes[1].neighbors = {0};
-    nodes[2].neighbors = {0,3};
-    nodes[3].neighbors = {0,2};
+    nodes[1].neighbors = {0,2,5};
+    nodes[2].neighbors = {0,1,3,4};
+    nodes[3].neighbors = {0,2,4,6};
+    nodes[4].neighbors = {2,3,5,6,7,8};
+    nodes[5].neighbors = {1,4,8};
+    nodes[6].neighbors = {3,4,7};
+    nodes[7].neighbors = {4,6};
+    nodes[8].neighbors = {4,5};
     
-    DFS(nodes,2);
+    DFS(nodes,0);
     
     return 0;
 }
